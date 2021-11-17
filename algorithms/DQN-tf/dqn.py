c0f06dc05ec19cd7418304beda7c6dab78987e9d
fuck <sellintang@Sellins-MacBook-Air.local>
2021-11-17 08:04:20 +0800

    up

--- algorithms/DQN-tf/dqn.py
+++ algorithms/DQN-tf/dqn.py
@@ -1,3 +1,5 @@
+import random
+
 import tensorflow as tf
 from replayMemory import Experience
 
@@ -11,13 +13,94 @@ dqn_model = tf.keras.Sequential(
     ]
 )
 
+class Dqn(tf.keras.Model):
+    def __init__(self, num_actions, env):
+        super(Dqn, self).__init__()
+        self.env = env
+        self.gamma = 0.9
+        self.epsilon = 1 # 1~0.1
+        self.step = 0
+        self.first_Conv2d = tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(78, 78, 1))
+        self.second_Conv2d = tf.keras.layers.Conv2D(64, (3,3), activation='relu')
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(64, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(num_actions)
+
+
+    def call(self, inputs, training=None, mask=None):
+        conv1_x = self.first_Conv2d(inputs)
+        conv2_x = self.second_Conv2d(conv1_x)
+        flatten_x = self.flatten(conv2_x)
+        dense1_x = self.dense1(flatten_x)
+        ouput_x = self.dense_output(dense1_x)
+
+        return ouput_x
+
+    def get_value(self, replayMemory):
+        if len(replayMemory) < 128:
+            return
+
+        training_set = replayMemory.sample(128)
+        # below operation will return Transition(state=(s0,s1...), action=(a0,a1...), next_state=(s'0, s'1...), reward=(r0, r1...))
+        batch = Experience(*zip(*training_set))
+
+        current_state_batch = tf.constant(batch.state)
+        self.action_batch = tf.constant(batch.action)
+        reward_batch = tf.constant(batch.reward)
+        next_state_batch = tf.constant(batch.next_state)
+
+        # calculate Q prediction value
+        # predict = self.call(current_state_batch, training=True)
+        # q_eval = tf.gather(predict, action_batch, batch_dims=action_batch.ndim)
+
+        # calculate target Q-a value
+        target = self.call(next_state_batch)
+        q_target = reward_batch + self.gamma * tf.math.reduce_max(target, 1)
+
+        return current_state_batch, tf.reshape(q_target, (q_target.shape[0], 1))
+
+    def train_step(self, data):
+
+        current_state_batch, y= data
+        with tf.GradientTape() as tape:
+            predict = self(current_state_batch, training=True)
+
+            y_pred = tf.gather(predict, self.action_batch, batch_dims=self.action_batch.ndim)
+            # Compute the loss value
+            # (the loss function is configured in `compile()`)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        # Update metrics (includes the metric that tracks the loss)
+        self.compiled_metrics.update_state(y, y_pred)
+        # Return a dict mapping metric names to current value
+        return {m.name: m.result() for m in self.metrics}
+
+    def select_action(self, state):
+        # select an action and execute according to epsilon-greedy policy
+        sample = random.random()
+        self.step += 1
+        eps_threshold = self.epsilon - 0.01 * self.step
+        state = tf.reshape(state, (1, *state.shape))
+        if sample > eps_threshold:
+            y = self.predict(state)
+            return int(tf.argmax(y, 1))
+        else:
+            return self.env.action_space.sample()
+
+
 class DQN(tf.keras.Model):
     def __init__(self, dqnmodel, batch_size, Gamma=0.9):
         super(DQN, self).__init__()
         self.batch_size = batch_size
         self.gamma = Gamma
         self.model = dqnmodel
-            
+
+
     def train_step(self, replayMemory):
         if len(replayMemory) < self.batch_size:
             return        
@@ -51,12 +134,19 @@ class DQN(tf.keras.Model):
         trainable_vars = self.model.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
         # Update weights
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.model.optimizer.apply_gradients(zip(gradients, trainable_vars))
         # Update metrics (includes the metric that tracks the loss)
-        self.compiled_metrics.update_state(y, y_pred)
+        self.model.compiled_metrics.update_state(y, y_pred)
         # Return a dict mapping metric names to current value
         return {m.name: m.result() for m in self.metrics}
         
     #def select_action(self, experience):
         
-
+if __name__ == "__main__":
+    dqn_model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    x = tf.random.normal((128, 78, 78, 1))
+    y = tf.random.uniform((128, 1))
+    ins = tf.random.normal((78,78,1))
+    dqn_model.predict(ins)
--- algorithms/DQN-tf/main.py
+++ algorithms/DQN-tf/main.py
@@ -1,7 +1,7 @@
 from replayMemory import replayMemory
 from object_image import get_screen
 import gym
-from dqn import DQN, dqn_model
+from dqn import Dqn
 if __name__ == "__main__":
     # Initialize replay memory D to capacity N
     replayMemory = replayMemory(10000)
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     
     env = gym.make('CartPole-v1')
     
-    agent = DQN(dqn_model, 128)
-    agent.compile(optimizer='rmsprop', loss='mse')
+    agent = Dqn(2, env)
+    agent.compile(optimizer='adam', loss='mse', metrics=["mae"])
 
     # play 100 episodes
     for i in range(100):
@@ -20,16 +20,19 @@ if __name__ == "__main__":
         print(i)
         total_reward = 0
         while True:
-            action = 0
+            action = agent.select_action(current_state)
             _, reward, done, _ = env.step(action)
             total_reward += reward
             next_state = get_screen(env)
 
             experience = (current_state, action, reward, next_state)
             replayMemory.append(current_state, action, reward, next_state)
-            print(len(replayMemory))
-            agent.train_step(replayMemory)
+            #print(len(replayMemory))
+            if (len(replayMemory) >= 128):
+                x, y = agent.get_value(replayMemory)
+                agent.fit(x,y, batch_size=128)
             if done:
                 break
             else:
                 current_state = next_state
+        print(i, " episode: ", total_reward)
